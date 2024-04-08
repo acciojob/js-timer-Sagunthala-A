df37bb318c58aff5d46222d12a412da290bd32d7
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-08 12:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-// write js code here if required
+const para = document.getElementById('timer');
+let date =  new Date();
+para.innerText = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
